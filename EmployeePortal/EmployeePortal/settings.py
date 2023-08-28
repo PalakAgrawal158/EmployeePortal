5c67e894598a90da65ba1685af97edbdb988d99d
yogesh.k@sankeysolutions.com <yogesh.k@sankeysolutions.com>
2023-08-28 16:01:45 +0100

    Changed setting and enivron removed

--- EmployeePortal/EmployeePortal/settings.py
+++ EmployeePortal/EmployeePortal/settings.py
@@ -17,10 +17,10 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-import environ
-# Initialise environment variables
-env = environ.Env()
-environ.Env.read_env()
+# import environ
+# # Initialise environment variables
+# env = environ.Env()
+# environ.Env.read_env()
 
 
 # Quick-start development settings - unsuitable for production
